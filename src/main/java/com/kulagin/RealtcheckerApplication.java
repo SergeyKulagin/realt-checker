8bcbdfaaa4cc4d00386c8ed617d925db9a96a5f7
sergey.kulagin <kulaginsp@gmail.com>
2018-06-05 16:08:02 +0300

    [realt checker]: encrypt the password

--- src/main/java/com/kulagin/RealtcheckerApplication.java
+++ src/main/java/com/kulagin/RealtcheckerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
